f3cb539a6eb7e34f628766106ca5712fa7ec129f
Daniel Bohen <bohen@usc.edu>
2022-06-12 15:45:53 -0700

    Update fixJson.py

--- fixJson.py
+++ fixJson.py
@@ -1,7 +1,35 @@
 import json, csv
 
 def main():
-    getCSV('epidural injection AND spine')
+    getCSV('"pain pump" AND "spine"')
+    getCSV('"baclofen pump" AND "spine"')
+    getCSV('"epidural injection" AND "spine"')
+    getCSV('"facet injection" AND "spine"')
+    getCSV('"cervical laminoplasty" AND "spine"')
+    
+    getCSV('"laminectomy" AND "spine"')
+    getCSV('"anterior lumbar interbody fusion"')
+    getCSV('"posterior lumbar interbody fusion"')
+    getCSV('oblique lateral interbody fusion')
+    getCSV('spinal cord stimulator')
+
+    getCSV('"Scoliosis" AND "spine"')
+    getCSV('"Spine OR spinal" AND "instrumentation"')
+    getCSV('Dorsal column stimulator')
+    getCSV('"Bone graft" AND "spine"')
+    getCSV('Osteotomy')
+
+    getCSV('"Anterior cervical discectomy" AND "fusion"')
+    getCSV('Cervical disc arthroplasty')
+    getCSV('Posterior cervical foraminotomy')
+    getCSV('Total disk replacement')
+    getCSV('Vertebroplasty')
+
+    getCSV('Kyphoplasty')
+    getCSV('"Burst fracture" AND "spine"')
+    getCSV('"Compression fracture" AND "spine"')
+    getCSV('"Fracture dislocation" AND "spine"')
+    getCSV('Dural tear')
 
 
 def getCSV(term):
